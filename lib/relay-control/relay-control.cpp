84677b0da197d48f81f310d9f78f85632867b339
Cristian Sulighetean <cristisulighetean@gmail.com>
2023-08-21 21:39:45 +0300

    Brought relay control in only one function and reverted the logic

--- lib/relay-control/relay-control.cpp
+++ lib/relay-control/relay-control.cpp
@@ -1,44 +1,57 @@
 #include "relay-control.h"
 
 const int relayPin = 0;
-unsigned long startTime = 0;
 bool relayOn = true;
+
+// Time variables
 const int maxDuration = 20 * 60 * 1000;
 const int minDuration = 5 * 60 * 1000;
 const int period = 60 * 60 * 1000; // 60 minutes
 
-unsigned long remainingTimeOn = 0; // Initialize remaining time for ON
-unsigned long remainingTimeOff = 0; // Initialize remaining time for OFF
+unsigned long startTime = 0;
+// Initialize remaining time
+long remainingTimeOn = 0; 
+long remainingTimeOff = 0;
 
 
 void relayControl() {
+  // Get current time
   unsigned long now = millis();
 
-  if (!manualMode) { // Automatic mode logic
+  // Sync relayOn with actual relay
+  if (relayOn) {
+    digitalWrite(relayOn, HIGH);
+  }
+  else {
+    digitalWrite(relayOn, LOW);
+  }
+
+  // Handle time update in automatic mode
+  if (!controlMode) { // Automatic mode logic
     if (relayOn) {
       // Update remaining time ON
       remainingTimeOn = onDuration - (now - startTime);
       // Turn off relay if remaining time is up
       if (remainingTimeOn <= 0) {
-        digitalWrite(relayPin, HIGH); // Relay logic is inverted
+        digitalWrite(relayPin, LOW);
         relayOn = false;
         startTime = now; // Reset start time
         remainingTimeOff = offDuration; // Reset remaining time OFF
-        Serial.println("Time out: Relay is disabled");
+        Serial.println("AUTO: time out - relay is disabled");
       }
     } else {
       // Update remaining time OFF
       remainingTimeOff = offDuration - (now - startTime);
       // If offDuration is reached, turn on the relay
       if (remainingTimeOff <= 0) {
-        digitalWrite(relayPin, LOW); // Relay logic is inverted
+        digitalWrite(relayPin, HIGH);
         relayOn = true;
         startTime = now;
         remainingTimeOn = onDuration; // Reset remaining time ON
-        Serial.println("Time in: Relay is enabled");
+        Serial.println("AUTO: time in - relay is enabled");
       }
     }
   } else { // Manual mode logic
-    digitalWrite(relayPin, relayOn ? LOW : HIGH);
+    digitalWrite(relayPin, relayOn ? HIGH : LOW);
   }
 }
